22d8d33d69b65775bd05ece1144822b85f975720
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-12-29 17:14:29 +0100

    update config

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'powerd6/specification',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/powerd6',
 			},
 			sidebar: [
 				{
